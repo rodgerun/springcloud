ddab3414fd1a5a427df2ab23c04836d1aaa15943
jiluo <jiluo@iflytek.com>
2020-01-31 21:15:55 +0800

    provider和consumer添加eureka

---
+++ 02-microservice-simple-provider-user/src/main/java/com/rodge/microservicesimpleprovideruser/EurekaProviderUserApplication.java
@@ -0,0 +1,13 @@
+package com.rodge.microservicesimpleprovideruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaProviderUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaProviderUserApplication.class, args);
+	}
+
+}
